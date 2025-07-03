ea937230f6eaaac933196aaab17953a4493fa7d2
Gustavo Cunha <dev@gustavocunha.dev>
2025-07-04 00:03:25 +0200

    feat: Add support to terragrunt command with --all option (#57)

--- dist/index.js
+++ dist/index.js
@@ -38339,6 +38339,9 @@ function parsePlanfileJSON(json2) {
 function planIsEmpty(plan) {
   return !plan.createdResources && !plan.recreatedResources && !plan.updatedResources && !plan.deletedResources;
 }
+function plansAreEmpty(plans) {
+  return !plans.map((plan) => planIsEmpty(plan)).some((result) => result === false);
+}
 function extractResourceContent(name, humanReadablePlan) {
   const lines = humanReadablePlan.split("\n");
   const resourceHeaderIndex = lines.findIndex((line) => line.startsWith(`  # ${name}`));
@@ -38413,6 +38416,24 @@ function internalRenderPlan(structuredPlan, humanReadablePlan) {
     deletedResources: extractResources(deletedResources, humanReadablePlan)
   };
 }
+async function renderTerraformPlan({
+  planfile,
+  terraformCommand,
+  options,
+  humanReadablePlanfile
+}) {
+  const structuredPlanfile = await exec.getExecOutput(terraformCommand, ["show", "-json", planfile], options).then((output) => JSON.parse(output.stdout)).then((json2) => parsePlanfileJSON(json2));
+  return [internalRenderPlan(structuredPlanfile, humanReadablePlanfile)];
+}
+async function renderTerragruntPlan({
+  planfile,
+  terraformCommand,
+  options,
+  humanReadablePlanfile
+}) {
+  const jsonPlans = await exec.getExecOutput(terraformCommand, ["show", "-json", planfile], options).then((output) => output.stdout.split("\n")).then((plans) => plans.filter((plan) => plan !== ""));
+  return jsonPlans.map((plan) => JSON.parse(plan)).map((json2) => parsePlanfileJSON(json2)).map((structuredPlanfile) => internalRenderPlan(structuredPlanfile, humanReadablePlanfile));
+}
 async function renderPlan({
   planfile,
   terraformCommand,
@@ -38422,9 +38443,25 @@ async function renderPlan({
     cwd: workingDirectory,
     silent: true
   };
-  const structuredPlanfile = await exec.getExecOutput(terraformCommand, ["show", "-json", planfile], options).then((output) => JSON.parse(output.stdout)).then((json2) => parsePlanfileJSON(json2));
   const humanReadablePlanfile = await exec.getExecOutput(terraformCommand, ["show", "-no-color", planfile], options).then((output) => output.stdout);
-  return internalRenderPlan(structuredPlanfile, humanReadablePlanfile);
+  try {
+    return await renderTerraformPlan({
+      planfile,
+      terraformCommand,
+      options,
+      humanReadablePlanfile
+    });
+  } catch (error45) {
+    if (error45 instanceof SyntaxError) {
+      return await renderTerragruntPlan({
+        planfile,
+        terraformCommand,
+        options,
+        humanReadablePlanfile
+      });
+    }
+  }
+  return [];
 }
 
 // src/comment.ts
@@ -38444,7 +38481,7 @@ ${content}
 }
 function renderBody(plan) {
   if (planIsEmpty(plan)) {
-    return "**\u2192 No Resource Changes!**";
+    return "";
   }
   let body = `**\u2192 Resource Changes: ${Object.keys(plan.createdResources ?? {}).length} to create, ${Object.keys(plan.updatedResources ?? {}).length} to update, ${Object.keys(plan.recreatedResources ?? {}).length} to re-create, ${Object.keys(plan.deletedResources ?? {}).length} to delete.**`;
   if (plan.createdResources) {
@@ -38466,11 +38503,14 @@ function renderBody(plan) {
   return body;
 }
 function renderMarkdown({
-  plan,
+  plans,
   header,
   includeFooter
 }) {
-  const body = renderBody(plan);
+  let body = plans.map((plan) => renderBody(plan)).filter((item) => item !== "");
+  if (body.length === 0) {
+    body = ["**\u2192 No Resource Changes!**"];
+  }
   let footer = "";
   if (includeFooter === void 0 || includeFooter === true) {
     footer = `
@@ -38485,7 +38525,7 @@ _Triggered by @${github.context.actor}`;
   }
   return `## ${header}
 
-${body}${footer}`;
+${body.join("\n\n")}${footer}`;
 }
 async function createOrUpdateComment({
   octokit,
@@ -38528,7 +38568,7 @@ async function run() {
     skipComment: core.getBooleanInput("skip-comment", { required: true })
   };
   const octokit = github2.getOctokit(inputs.token);
-  const plan = await core.group(
+  const plans = await core.group(
     "Render plan",
     () => renderPlan({
       planfile: inputs.planfile,
@@ -38537,15 +38577,15 @@ async function run() {
     })
   );
   const planMarkdown = await core.group("Render plan diff markdown", async () => {
-    const markdown = renderMarkdown({ plan, header: inputs.header });
+    const markdown = renderMarkdown({ plans, header: inputs.header });
     core.setOutput("markdown", markdown);
-    core.setOutput("empty", planIsEmpty(plan));
+    core.setOutput("empty", plansAreEmpty(plans));
     return markdown;
   });
   await core.group("Adding plan to step summary", async () => {
     await core.summary.addRaw(planMarkdown).write();
   });
-  if (!inputs.skipComment && (!inputs.skipEmpty || !planIsEmpty(plan)) && ["pull_request", "pull_request_target"].includes(github2.context.eventName)) {
+  if (!inputs.skipComment && (!inputs.skipEmpty || !plansAreEmpty(plans)) && ["pull_request", "pull_request_target"].includes(github2.context.eventName)) {
     await core.group("Render comment", () => {
       return createOrUpdateComment({ octokit, content: planMarkdown });
     });

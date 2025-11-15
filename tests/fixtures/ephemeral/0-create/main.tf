5bbe42feae5d505d9101163e49af9a0fd29a6b60
Oliver Borchert <oliver.borchert@quantco.com>
2025-11-16 00:46:02 +0100

    Eph

--- tests/fixtures/ephemeral/0-create/main.tf
+++ tests/fixtures/ephemeral/0-create/main.tf
@@ -1,7 +1,14 @@
 terraform {
+  backend "local" {
+    path = "../.tfstate"
+  }
+
   required_providers {
     random = {
       source = "hashicorp/random"
+    }
+    null = {
+      source = "hashicorp/null"
     }
   }
 }
@@ -9,4 +16,10 @@ terraform {
 ephemeral "random_password" "test" {
   length  = 16
   special = true
+}
+
+resource "null_resource" "run_command" {
+  provisioner "local-exec" {
+    command = "echo 'Test: ${ephemeral.random_password.test.result}'"
+  }
 }
